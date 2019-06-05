694eb4903ef169a23ad018892f92e089a8b85cf1
1559924775 <1559924775@qq.com>
2019-06-05 15:56:08 +0800

    引入了TCC-Trasaction依赖，增加了分布式事务

--- Crawler/src/main/resources/db.sql
+++ Crawler/src/main/resources/db.sql
@@ -1,4 +1,4 @@
-use spider;
+USE SPIDER;
 CREATE TABLE douban1(
     id VARCHAR(64),
     name VARCHAR(50),
@@ -13,6 +13,7 @@ CREATE TABLE douban1(
 
 create table douban2(
     id varchar(64),  -- 关联douban1的id
+    fid varchar(64), -- 关联douban1的id
     commentnumber varchar(50),
     score varchar(50),
     date varchar(50)
@@ -26,6 +27,7 @@ create table zongheng1(
 );
 create table zongheng2(
     id varchar(64),
+    fid varchar(64), -- 关联zongheng1的id
     recommend varchar(50),
     click varchar(50),
     date varchar(50)
@@ -35,3 +37,21 @@ create table startUrl(
   name varchar(20),-- 任务名称
   url varchar(300)
 );
+
+-- tcc事务用这个表来记录事务情况
+CREATE TABLE `TCC_TRANSACTION` (
+  `TRANSACTION_ID` int(11) NOT NULL AUTO_INCREMENT,
+  `DOMAIN` varchar(100) DEFAULT NULL,
+  `GLOBAL_TX_ID` varbinary(32) NOT NULL,
+  `BRANCH_QUALIFIER` varbinary(32) NOT NULL,
+  `CONTENT` varbinary(8000) DEFAULT NULL,
+  `STATUS` int(11) DEFAULT NULL,
+  `TRANSACTION_TYPE` int(11) DEFAULT NULL,
+  `RETRIED_COUNT` int(11) DEFAULT NULL,
+  `CREATE_TIME` datetime DEFAULT NULL,
+  `LAST_UPDATE_TIME` datetime DEFAULT NULL,
+  `VERSION` int(11) DEFAULT NULL,
+  `IS_DELETE` tinyint(1) DEFAULT 0 NOT NULL,
+  PRIMARY KEY (`TRANSACTION_ID`),
+  UNIQUE KEY `UX_TX_BQ` (`GLOBAL_TX_ID`,`BRANCH_QUALIFIER`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
